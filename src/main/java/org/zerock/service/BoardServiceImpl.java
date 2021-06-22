cf6e8d9a2440557a2be4a21febbb4becafe374db
Dmin3 <ekdrnzld@gmail.com>
2021-06-22 18:42:52 +0900

    210622

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,50 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor // 생성자 자동생성
+@Service // @Component 포함
+public class BoardServiceImpl implements BoardService {
+	
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		
+		// update 성공시 하나의 레코드가 잘들어간거이기때문에 1을 리턴한다
+		// update 타입이 Integer 이다. 
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(long bno) {
+		
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		
+		return mapper.getList();
+	}
+
+	
+}
